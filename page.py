2d2354d0eda8eda32855c1b445a1e909ac194b69
Michael JasonSmith <mpj17@onlinegroups.net>
2008-12-17 04:23:24 +0000

    Adding a page view.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/GSContentManager/trunk%404632

--- page.py
+++ page.py
@@ -1,4 +1,5 @@
 import os
+from zope.component import adapts, createObject
 from interfaces import *
 from time import gmtime, time, strftime
 from zope.interface import implements, alsoProvides
@@ -9,183 +10,96 @@ from OFS.OrderedFolder import OrderedFolder
 from lxml import etree
 from StringIO import StringIO
 import interfaces
+from Products.Five import BrowserView
 
 from page_history import GSPageHistory
 
-class GSContentPage(object):
-    """ Wraps a page template to implement a 'content' attribute 
-    that formlib can use transparently, even though the content 
-    attribute actually maps to the page template content rather than 
-    a property on the object"""
+
+class GSContentPage(BrowserView):
+    """The view of a version-controlled content page."""
     
     implements(IGSContentPage)
     
     CONTENT_TEMPLATE = 'content_en'
     initial_content_file = 'content.html'
     
-    def __init__ (self, context, pageId=None):
-        self.context = context
-        self.pageHistory = GSPageHistory(context)
-        
+    def __init__(self, context, request):
+        assert context
+        assert request
+        BrowserView.__init__(self, context, request)
+        self.siteInfo = createObject('groupserver.SiteInfo',
+          self.context)
+        self.pageHistory = hist = GSPageHistory(context)
         self.interface = interface = getattr(interfaces, 'IGSContentPage')
-        enforce_schema(self.context, IGSContentPage)
-        if False: # Fix later
-            # If we're adding a new page, create the page then 
-            # enforce the schema.
-            if pageId:
-                # Check that we don't have an existing page with 
-                # this ID in the container.
-                if hasattr(self.context, pageId):
-                    m = 'A page with id %s already exists' % pageId
-                    self.status = {'error': True, 'msg': m}
-                else:
-                    # All good, so create the folder.
-                    self.context.manage_addOrderedFolder(pageId)
-                    folder = getattr(self.context, pageId)
-                    alsoProvides(folder, IGSContentFolder)
-                    
-                    # Create the content page.
-                    content_page = interface(folder)
-                    enforce_schema(content_page, IGSContentPage)
-                    self.context = folder
-
-                    m = 'Added page with id %s.' % pageId
-                    self.status = {'error': False, 'msg': m}
-
-        
-    def add_to_history (self):
-        """ Creates a history entry for the context object """
-        history_obj_name = ''# --=mpj17=-- Fix self.new_version_id()
-        if not getattr(self.context.aq_explicit, history_obj_name, None):
-            content_obj = self.pageHistory.get_current_version()
-            assert content_obj
-            history_obj = self.context.manage_clone(content_obj, history_obj_name)  
-            
-            # Add the last editor (as indicated by the last history entry 
-            # for the page) as a property on the history entry just 
-            # created.
-            last_editor = self.get_last_editor()
-            history_obj.manage_addProperty('editor', last_editor, 'string')
-            
-            # If the currently published revision is the latest one, set 
-            # the published revision to the newly created historical 
-            # revision.
-            if getattr(self.context, 'published_revision', None) == content_obj.getId():
-                self.context.published_revision = history_obj_name
-            
-            return history_obj
+        self.__userInfo = None
     
-    def copy_revision_to_current (self, revision):
-        """ Copy the revision with the specified ID to the current revision """
-        assert False, "Replace the call to copy_revision_to_current"  
-        assert type(revision) in (str, unicode)
+    @property
+    def userInfo(self):
+        if self.__userInfo == None:
+            self.__userInfo = createObject('groupserver.LoggedInUser',
+              self.context)
+        retval = self.__userInfo
+        assert retval
+        return retval
         
-        newVersionId = self.new_version_id()
-        newVersion = self.context.manage_clone(revision, newVersionId)          
-
-    def publish_revision (self, revision):
-        """ Publish the specified revision """
-        # Update the published revision
-        # --=mpj17=-- Change how publishing works.
-        self.published_revision = revision
-    
-    def get_last_editor (self):
-        """ Get the last editor of the context object """
-        try:
-            # Get the last history entry
-            r = self.context._p_jar.db().history(self.context._p_oid, None, 1)
-            if r:
-                editor = r[0]['user_name'].split(' ')[1]
-                return editor
-        except:
-            return ''
-    
-    #------------------------------------------------------------------------
-    # Properties
-    #------------------------------------------------------------------------
-
-    # Content property
-    def _setContent(self, value):
-        # Save the content to the content object
-        template = self.pageHistory.get_current_version()
-        if template:
-            template.write('<content>%s</content>' % value)
+    @property
+    def version(self):
+        '''The version of the page to display. 
         
-    def _getContent(self):
-        template = self.pageHistory.get_current_version()
-        if template:
-            doc = etree.fromstring(template._text)
-            tree = doc.xpath('//content/*')
-            return ''.join(map(lambda x: etree.tostring(x), tree))
-    
-    def fix_id (self, id):
-        str = ''
-        legal = string.digits + string.ascii_lowercase
-        id = id.lower()
-        for c in range(0, len(id)):
-            char = id[c]
-            str += legal.find(char) >= 0 and char or ''
-        return (str)    
-
-    content = property(_getContent, _setContent)    
-
-    # ID property
-    def _setId(self, value):
-        try:
-            self.context.id = self.fix_id(value)
-        except:
-            pass
+        If the user can view the page history then he or she will be
+        shown the requested version (set in form.version), or the
+        published version of the page.
         
-    def _getId(self):
+        If the user cannot view the page history then the user will
+        be shown only the published version of the page.
+        '''
+        pId = self.pageHistory.published.getId()
+        uo = self.userInfo.user
+        canViewOld = uo.has_permission('View History', self.context)
+        if canViewOld:
+            retval = self.request.form.get('form.version',  pId)
+        else:
+            retval = pId    
+        assert retval
+        return retval
+        
+    @property
+    def content(self):
+        '''Gets the content from the requested version of the page.
+        '''
+        retval = self.pageHistory[self.version]()
+        assert type(retval) in (str, unicode)
+        return retval
+
+    def getId(self):
+        '''The ID of the page is the ID of the folder that contains
+        the page.
+        '''
         return self.context.id
     
-    id = property(_getId, _setId)    
+    @property
+    def title(self):
+        '''Gets the title from the requested version of the page.
+        '''
+        retval = self.pageHistory[self.version].title_or_id()
+        assert type(retval) in (str, unicode)
+        return retval
 
-    # Title property
-    def _setTitle(self, value):
-        try:
-            self.context.title = value
-        except:
-            pass
-        
-    def _getTitle(self):
-        return self.context.title
-    
-    title = property(_getTitle, _setTitle)    
-
-    # Hidden property
-    def _setHidden(self, value):
-        try:
-            self.context.hidden = value
-        except:
-            pass
-        
-    def _getHidden(self):
-        return self.context.hidden
-    
-    hidden = property(_getHidden, _setHidden)    
-
-    # Description property
-    def _setDescription(self, value):
-        try:
-            self.context.description = value
-        except:
-            pass
-        
-    def _getDescription(self):
-        return self.context.description
-
-    description = property(_getDescription, _setDescription)
-
-    # Published_revision property
-    def _setPublishedRevision(self, value):
-        try:
-            self.context.published_revision = value
-        except:
-            pass
-        
-    def _getPublishedRevision(self):
-        return self.context.published_revision
-
-    published_revision = property(_getPublishedRevision, _setPublishedRevision)
+    @property
+    def hidden(self):
+        '''Returns the hidden value of the *entire* folder, not the
+        version.
+        '''
+        retval = self.context.hidden
+        assert type(retval) == bool
+        return retval
+                
+    @property
+    def editor(self):
+        '''Returns the name of the last editor.
+        '''
+        assert hasattr(self.pageHistory[self.version], 'editor')
+        retval = self.pageHistory[self.version].editor
+        assert type(retval) in (str, unicode)
+        return retval
 
