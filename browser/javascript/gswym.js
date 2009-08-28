2a2f00831306db8a3724c0c1c570f027ddbb178e
Michael JasonSmith <mpj17@onlinegroups.net>
2009-08-28 15:53:27 +1000

    Initial attempt at a GroupServer skin.

---
+++ browser/javascript/gswym.js
@@ -0,0 +1,10 @@
+
+var options = {
+  skinPath: '/++resource++gswymskin/gs/',
+  skin: 'gs',
+};
+jQuery(document).ready( function() {
+    jQuery('#form\\.actions\\.change').addClass('wymupdate');
+    jQuery('.wymeditor').wymeditor(options);
+});
+
