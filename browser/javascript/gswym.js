cc8e4d809faf66f4da1abbb1ece940309ee67491
Michael JasonSmith <mpj17@onlinegroups.net>
2009-08-31 11:35:52 +1000

    Using the 0.5 RC 1 version of the WYMeditor.

--- browser/javascript/gswym.js
+++ browser/javascript/gswym.js
@@ -2,6 +2,7 @@
 var options = {
   skinPath: '/++resource++gswymskin/gs/',
   skin: 'gs',
+  langPath: '/++resource++wymeditor-20090831/wymeditorlang/',
   dialogFeatures: "menubar=no,titlebar=no,toolbar=no,resizable=no,width=504,height=504,top=0,left=0",
 };
 jQuery(document).ready( function() {
